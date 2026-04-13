9f37dca56399d5e2efd1356fd1418c99bae36293
phillipcs <pgibsoncs@gmail.com>
2026-04-13 08:36:23 -0500

    Bulk sync: 117 submissions

---
+++ Data Structures & Algorithms/find-target-in-rotated-sorted-array/submission-1.py
@@ -0,0 +1,25 @@
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        l, r = 0, len(nums) - 1
+
+        while l <= r:
+            m = (l + r) // 2
+
+            if nums[m] == target:
+                return m
+            
+            # left half sorted
+            if nums[l] <= nums[m]:
+                if nums[m] < target or nums[l] > target:
+                    l = m + 1
+                else:
+                    r = m - 1
+            
+            # right half sorted
+            else:
+                if nums[m] > target or nums[r] < target:
+                    r = m - 1
+                else:
+                    l = m + 1
+            
+        return -1
---
+++ Data Structures & Algorithms/level-order-traversal-of-binary-tree/submission-0.py
@@ -0,0 +1,28 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution:
+    def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+        if not root:
+            return []
+
+        q = collections.deque([root])
+        res = []
+
+        while q:
+            levels = []
+            for i in range(len(q)):
+                curr = q.popleft()
+                levels.append(curr.val)
+                if curr.left:
+                    q.append(curr.left)
+                if curr.right:
+                    q.append(curr.right)
+            res.append(levels)
+        
+        return res
+        
